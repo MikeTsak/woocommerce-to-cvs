196aaafd8a76e208fe72cae42c79ebd0f8e81cfa
Mike <32255755+MikeTsak@users.noreply.github.com>
2023-12-05 16:07:50 +0200

    Added date selectio

--- woocommerce-to-cvs.py
+++ woocommerce-to-cvs.py
@@ -27,35 +27,44 @@ consumer_key = os.getenv("WOO_CONSUMER_KEY")
 consumer_secret = os.getenv("WOO_CONSUMER_SECRET")
 api_endpoint = f"{store_url}/wp-json/wc/v3/orders"
 
-# Making the request
-print("Fetching orders...")
-response = requests.get(api_endpoint, auth=(consumer_key, consumer_secret), timeout=60)
+# User input for date range
+start_date = input("Enter the start date (YYYY-MM-DD): ")
+end_date = input("Enter the end date (YYYY-MM-DD): ")
+
+# Making the request with date range
+params = {
+    'after': start_date + 'T00:00:00',
+    'before': end_date + 'T23:59:59',
+    'per_page': 100  # Adjust per_page to the expected number of orders, or implement pagination
+}
+
+print(f"Fetching orders from {start_date} to {end_date}...")
+response = requests.get(api_endpoint, auth=(consumer_key, consumer_secret), params=params, timeout=60)
 orders = response.json()
 print(f"Fetched {len(orders)} orders.")
 
 # Process orders data
 orders_data = []
 for order in orders:
-    for item in order['line_items']:
-        email = order['billing']['email'] if 'email' in order['billing'] and order['billing']['email'] else '-'
-        order_data = {
-            'Name': order['billing']['first_name'] + " " + order['billing']['last_name'],
-            'Postal Code': order['billing']['postcode'],
-            'City': order['billing']['city'],
-            'Address': order['billing']['address_1'],
-            'Phone Number': order['billing']['phone'],
-            'Quantity of Products': item['quantity'],
-            'Price': order['total'],
-            'Order Date': order['date_created'],
-            'Email': email,
-        }
-        orders_data.append(order_data)
+    email = order['billing']['email'] if 'email' in order['billing'] and order['billing']['email'] else '-'
+    order_data = {
+        'Name': order['billing']['first_name'] + " " + order['billing']['last_name'],
+        'Postal Code': order['billing']['postcode'],
+        'City': order['billing']['city'],
+        'Address': order['billing']['address_1'],
+        'Phone Number': order['billing']['phone'],
+        'Quantity of Products': sum(item['quantity'] for item in order['line_items']),
+        'Price': order['total'],
+        'Order Date': order['date_created'],
+        'Email': email,
+    }
+    orders_data.append(order_data)
 print("Orders data processed.")
 
 # Convert to DataFrame
 df = pd.DataFrame(orders_data)
 
-#Get current date and time
+# Get current date and time
 now = datetime.now()
 formatted_date_time = now.strftime("%Y-%m-%d-%H-%M-%S")
 
@@ -63,3 +72,4 @@ formatted_date_time = now.strftime("%Y-%m-%d-%H-%M-%S")
 filename = f"orders-{formatted_date_time}.csv"
 df.to_csv(filename, index=False)
 print(f"Orders saved to {filename} successfully.")
+input("Press Enter to exit...")
